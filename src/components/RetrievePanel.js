3dcb09ccf18cb6b62ea84c15542535a120e5ec10
ABDUL MUSAVVIR <abdulmusavvir2000@gmail.com>
2026-05-27 12:14:14 +0530

    Update RetrievePanel.js

--- src/components/RetrievePanel.js
+++ src/components/RetrievePanel.js
@@ -26,7 +26,7 @@ function RetrievePanel() {
     setFoundFile(null);
 
     try {
-      const response = await axios.post('http://localhost:5000/api/retrieve', {
+      const response = await axios.post('https://fylfly-backend-2.onrender.com', {
         code: code.trim()
       });
 
